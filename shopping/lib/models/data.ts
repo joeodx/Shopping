a48772cf8a34e5a55f9f624d81b934bddc718d75
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-06 16:10:04 +0000

    Versioning untracked files

--- shopping/lib/models/data.ts
+++ shopping/lib/models/data.ts
@@ -41,6 +41,63 @@ const data = {
             numReviews:"15",
             countInStock:"25",
             },
+
+            {
+            
+                name: "Arsenal Shirt",
+                slug: "Football Shirt",
+                brand:"Adidas",
+                image:"/images/arsenal.jpg",
+                price:"70",
+                description:"An Arsenal Home shirt 2023/2024",
+                category:"Football",
+                rating:"5 Stars",
+                numReviews:"15",
+                countInStock:"25",
+                },
+
+                {
+            
+                    name: "Arsenal Shirt",
+                    slug: "Football Shirt",
+                    brand:"Adidas",
+                    image:"/images/arsenal.jpg",
+                    price:"70",
+                    description:"An Arsenal Home shirt 2023/2024",
+                    category:"Football",
+                    rating:"5 Stars",
+                    numReviews:"15",
+                    countInStock:"25",
+                    },
+
+                    {
+            
+                        name: "Arsenal Shirt",
+                        slug: "Football Shirt",
+                        brand:"Adidas",
+                        image:"/images/arsenal.jpg",
+                        price:"70",
+                        description:"An Arsenal Home shirt 2023/2024",
+                        category:"Football",
+                        rating:"5 Stars",
+                        numReviews:"15",
+                        countInStock:"25",
+                        },
+
+                        {
+            
+                            name: "Arsenal Shirt",
+                            slug: "Football Shirt",
+                            brand:"Adidas",
+                            image:"/images/arsenal.jpg",
+                            price:"70",
+                            description:"An Arsenal Home shirt 2023/2024",
+                            category:"Football",
+                            rating:"5 Stars",
+                            numReviews:"15",
+                            countInStock:"25",
+                            },
+                            
     ],
 }
 
