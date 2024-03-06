5a42c398f9758597a225ecef37092a5b84b9867f
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-06 17:08:55 +0000

    added

--- shopping/app/(front)/Product/[slug]/page.tsx
+++ shopping/app/(front)/Product/[slug]/page.tsx
@@ -15,11 +15,10 @@ params: { slug: string }
   return <>
   <div className="my-2">
     <Link href="/">back to products</Link>
-
   </div>
-  <div className="grid md:frid-cols-4 md:gap-3">
+  <div className="grid md:grid-cols-4 md:gap-3">
     <div className="md:col-span-2">
-        <Image 
+      <Image 
         src={product.image}
         alt={product.name}
         width={640}
@@ -29,10 +28,9 @@ params: { slug: string }
             width: "100%",
             height: "auto",
         }}
-        ></Image>
-   
-  </div>
-  <div>
+      ></Image>
+     </div>
+     <div>
     <ul className="space-y-4">
         <li>
             <h1 className="text-xl">{product.name}</h1>
