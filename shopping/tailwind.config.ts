b35c4269607780937193a5c772943916c488b4ad
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-06 15:03:08 +0000

    addedConfig

--- shopping/tailwind.config.ts
+++ shopping/tailwind.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from "tailwindcss";
-
+module.exports = {
+  //...
+  plugins: [require("daisyui")],
+}
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,6 +18,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("daisyui")],
+
 };
 export default config;
