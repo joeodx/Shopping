8cc12dd4402fb1623f1e372eca1f8f589b8fbb8f
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-06 16:55:53 +0000

    addedIPag

--- shopping/app/(front)/Product/[slug]/page.tsx
+++ shopping/app/(front)/Product/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import data from "@/lib/models/data"
+import Link from "next/link"
+import Image from "next/image"
 
 export default function ProductDetails({ 
 params, 
@@ -10,7 +12,36 @@ params: { slug: string }
     if (!product) {
         return <div>Product not found</div>
     }
-  return <div>page</div>
+  return <>
+  <div className="my-2">
+    <Link href="/">back to products</Link>
+
+  </div>
+  <div className="grid md:frid-cols-4 md:gap-3">
+    <div className="md:col-span-2">
+        <Image 
+        src={product.image}
+        alt={product.name}
+        width={640}
+        height={640}
+        sizes="100vw"
+        style={{
+            width: "100%",
+            height: "auto",
+        }}
+        ></Image>
+   
+  </div>
+  <div>
+    <ul className="space-y-4">
+        <li>
+            <h1 className="text-xl">{product.name}</h1>
+        </li>
+    </ul>
+    
+  </div>
+  </div>
+  </>
   }
 
 
