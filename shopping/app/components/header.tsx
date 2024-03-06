c429143113a6eb867337a26a5c89e719cadda199
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-06 14:52:55 +0000

    addedHeader

---
+++ shopping/app/components/header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header>
+        <nav>
+    <div className="navbar justify-between bg-base-300">
+        <link href="/" className='btn btn-ghost text-lg'>
+            JOMAZON
+
+        </link>
+    </div>
+    </nav>
+    </header>
+  )
+}
+
+export default Header
